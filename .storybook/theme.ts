6e51a272bb5e7f8dbc97d63c7a676c54c5c0278f
Gabao-Farias-Builders <gabriel.farias@platformbuilders.io>
2021-07-22 02:30:56 -0300

    added custom theme based on tdc

---
+++ .storybook/theme.ts
@@ -0,0 +1,21 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+
+  colorSecondary: '#009945',
+
+  appBg: '#101110',
+  appContentBg: '#202120',
+  appBorderColor: '#009945',
+
+  textColor: '#fff',
+
+  barTextColor: '#e6e6e6',
+  barSelectedColor: '#fff',
+  barBg: '#1a1a1a',
+
+  brandTitle: 'TDC Transformation',
+  brandImage: 'https://thedevconf.com/img/logos/logo-tdc-horizontal-navbar-inverse.png',
+  brandUrl: 'https://thedevconf.com/tdc/2021/index.html'
+});
